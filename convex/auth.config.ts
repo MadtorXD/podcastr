66656a112d2ffd462f21bb9a7855229fc21b2429
MadtorXD <153019410+MadtorXD@users.noreply.github.com>
2024-11-22 17:41:25 +0530

    Add files via upload

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://trusting-pangolin-91.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
